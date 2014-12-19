0c94248bed5ef076b0d71e47daaddf13a9def925
Dovakin94 <Dovakin940@gmail.com>
2014-12-19 10:11:09 +0200

    Add all src files

---
+++ KeyGenerator.java
@@ -0,0 +1,22 @@
+import java.security.SecureRandom;
+
+
+public class KeyGenerator {
+
+	/**
+	 * @param args
+	 */
+	public void init(SecureRandom random) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void init(int keysize) {
+		// TODO 
+	}
+	
+	public void init(int keysize, SecureRandom random){
+		// TODO
+	}
+
+}
